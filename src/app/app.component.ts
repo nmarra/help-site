5ebd93567c67b9158a78a088c539c37cfa10be3b
Nick Marra <nickmarra@gmail.com>
2018-04-15 21:19:31 -0500

    Image compression and carousel implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+import {NgxCarousel} from 'ngx-carousel';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -9,14 +10,40 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   logo = "assets/logo.jpg";
+
+  public imageCarousel: NgxCarousel;
+  public carouselTileItems: Array<any> = [];
   images = [
-    {src: "assets/help1_Lwide.jpg"},
-    {src: "assets/help2_Lwide.jpg"},
-    {src: "assets/help3_Lwide.jpg"},
-    {src: "assets/help4_Lwide.jpg"},
-    {src: "assets/help5_Lwide.jpg"},
-    {src: "assets/help6_Lwide.jpg"},
-    {src: "assets/help8_Lwide.jpg"},
-    {src: "assets/help9_Lwide.jpg"},
-  ]
+    "assets/help1_Lwide.jpg",
+    "assets/help2_Lwide.jpg",
+    "assets/help3_Lwide.jpg",
+    "assets/help4_Lwide.jpg",
+    "assets/help5_Lwide.jpg",
+    "assets/help6_Lwide.jpg",
+    "assets/help8_Lwide.jpg",
+    "assets/help9_Lwide.jpg"
+  ];
+
+  ngOnInit() {
+    this.imageCarousel = {
+      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: true,
+      loop: true,
+      custom: 'banner'
+    };
+    this.carouselLoad();
+  }
+
+  public carouselLoad() {
+    for (let i = 0; i < this.images.length; i++) {
+      this.carouselTileItems.push(this.images[i]);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatSidenavModule, MatToolbarModule, MatIconModule,
   MatButtonModule, MatMenuModule, MatListModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {NgxCarouselModule} from 'ngx-carousel';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, MatListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, NgxCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
