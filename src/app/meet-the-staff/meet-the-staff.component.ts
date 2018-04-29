eed672b3c099fb7c3efbc12d5c4d58009ddcec46
Nick Marra <nickmarra@gmail.com>
2018-04-28 22:48:14 -0500

    Resized home page images, updated some carousel stuff and the meet the staff page order.

--- src/app/meet-the-staff/meet-the-staff.component.ts
+++ src/app/meet-the-staff/meet-the-staff.component.ts
@@ -22,11 +22,6 @@ export class MeetTheStaffComponent implements OnInit {
       bio: 'Micki has volunteered at HELP since 2006, and served on the board since 2009. She and her mare, Shasta, enjoy riding trails around her home in Taylor.'
     },
     {
-    name: 'Beau Whitaker, DVM',
-    image: 'assets/staff/BeauWhitaker.jpg',
-     bio: 'Dr. Beau Whitaker grew up near Nashville, Tennessee working on his family\'s farm. Prior to attending veterinary school, he trained halter horses professionally in north Texas. Beau graduated from Texas A&M University College of Veterinary Medicine in 2005. Upon graduation, he completed an internship at Arizona Equine in Gilbert, Arizona. Subsequently, Beau then worked as a veterinarian at the 6666\'s Ranch in Guthrie, Texas. Beau moved to Salado, Texas, and joined Dr. T’s Equine Clinic (now Brazos Valley Equine Hospital, Salado) in 2007, where he established a busy lameness and sports medicine clientele. Beau is interested in equine rehabilitation and received his Certification in Equine Rehabilitation Therapy (CERT) in 2014 from the University of Tennessee. '
-    },
-    {
     name: 'Kirk Zippe',
     image: 'assets/staff/KirkZippe.jpg',
       bio: 'Kirk has been a volunteer at HELP since 1999 and  President of the board since 2009. He welcomes you to HELP and looks forward to sharing the  wonderful and memorable experiences you and your child will have riding with  us.  His son Auggie started riding with HELP in 1999 at age 2, and rode there for 13 years. He has since graduated and continues to ride at an  equestrian center without assistance. Now 20, he continues to learn to ride English and is trotting and jumping on his own.'
@@ -40,6 +35,11 @@ export class MeetTheStaffComponent implements OnInit {
     name: 'Caitlin Admire',
     image: 'assets/staff/CaitlinAdmire.jpg',
       bio: 'Caitlin has been around horses for over 20 years. She grew up showing AQHA and hunter/jumpers with her big redheaded ex-racehorse, Dollar. Her passion for therapeutic riding began in college when she worked at a stable that also housed a therapeutic riding program. Along with riding for the stable, she began helping with the therapy classes, carrying out lesson plans provided by the PATH certified head trainer.  She went to college at Kansas State University and earned her Master’s Degree in Landscape Architecture and a certificate in Pre-Veterinary Equine Science. Her first internship in the architecture world was with an equestrian architecture firm that focused solely on designing equestrian facilities throughout the country. She now works as an urban designer and campus planner for the local office of international architecture firm, Perkins+Will.  Caitlin has lived in Austin since 2014, currently serves as a board member for HELP and rides just around the corner at Manor Equestrian Center.  When she’s not at the barn, Caitlin can be found with her husband working on their renovated 1970’s bungalow in South Austin, spoiling their two rescue pups, and supporting the local food and beverage scene around town.'
+    },
+    {
+      name: 'Beau Whitaker, DVM',
+      image: 'assets/staff/BeauWhitaker.jpg',
+      bio: 'Dr. Beau Whitaker grew up near Nashville, Tennessee working on his family\'s farm. Prior to attending veterinary school, he trained halter horses professionally in north Texas. Beau graduated from Texas A&M University College of Veterinary Medicine in 2005. Upon graduation, he completed an internship at Arizona Equine in Gilbert, Arizona. Subsequently, Beau then worked as a veterinarian at the 6666\'s Ranch in Guthrie, Texas. Beau moved to Salado, Texas, and joined Dr. T’s Equine Clinic (now Brazos Valley Equine Hospital, Salado) in 2007, where he established a busy lameness and sports medicine clientele. Beau is interested in equine rehabilitation and received his Certification in Equine Rehabilitation Therapy (CERT) in 2014 from the University of Tennessee. '
     }
   ]
 
--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -1,33 +1,11 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, QueryList, ViewChildren} from '@angular/core';
-import {animate, group, query, state, style, transition, trigger} from '@angular/animations';
+import {AfterViewInit, Component, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
-  // animations: [
-    // trigger('slide', [
-    //   transition(":increment", group([
-    //     query(':enter', [
-    //       style({ left: '100%' }),
-    //       animate('0.5s ease-out', style('*'))
-    //     ],{ optional: true }),
-    //     query(':leave', [
-    //       animate('0.5s ease-out', style({ left: '-100%' }))
-    //     ],{ optional: true })
-    //   ])),
-    //   transition(":decrement", group([
-    //     query(':enter', [
-    //       style({ left: '-100%' }),
-    //       animate('0.5s ease-out', style('*'))
-    //     ],{ optional: true }),
-    //     query(':leave', [
-    //       animate('0.5s ease-out', style({ left: '100%' }))
-    //     ],{ optional: true })
-    //   ]))
-    // ])]
 })
-export class CarouselComponent implements OnInit, OnDestroy, AfterViewInit {
+export class CarouselComponent implements OnInit, OnDestroy {
   @Input() _images: any[];
   @Input() interval: number = 3000;
 
@@ -45,20 +23,10 @@ export class CarouselComponent implements OnInit, OnDestroy, AfterViewInit {
     //   clearInterval(this.intervalId);
     // }
   }
-  ngAfterViewInit() {
-  }
 
   get images() {
     console.log(this.currentIndex, [this._images[this.currentIndex]]);
     return [this._images[this.currentIndex]];
-  }
-
-  prev() {
-    this.currentIndex = this.currentIndex === 0 ? this._images.length - 1 : this.currentIndex -1;
-  }
-
-  next() {
-    this.currentIndex = this.currentIndex === this._images.length - 1 ? 0 : this.currentIndex +1;
   }
 
   setCurrentImage(i) {
